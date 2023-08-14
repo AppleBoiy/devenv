b1d8972a17b09352c5cdf9ad7e8a85662c76ba02
Chaipat J <work.chaipat@gmail.com>
2023-08-14 14:48:06 +0700

    feat(dotfiles): add backup config script.

---
+++ backup_config.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script: backup_configs.sh
+# Description: A script to backup zsh configuration files.
+# Author: AppleBoiy
+# Date: August 14, 2023
+
+# Define source and destination paths for configuration files
+SOURCE_ZSHRC=~/.zshrc
+SOURCE_P10K=~/.p10k.sh
+SOURCE_NVIM=~/.config/nvim/lua/custom
+DESTINATION_DIR=./config_backups
+
+# Create a directory to store the backup files if it doesn't exist
+mkdir -p "$DESTINATION_DIR"
+
+# Backup .zshrc
+if [ -f "$SOURCE_ZSHRC" ]; then
+  cp "$SOURCE_ZSHRC" "$DESTINATION_DIR"
+  echo "Backed up .zshrc to $DESTINATION_DIR"
+fi
+
+# Backup .p10k.sh
+if [ -f "$SOURCE_P10K" ]; then
+  cp "$SOURCE_P10K" "$DESTINATION_DIR"
+  echo "Backed up .p10k.sh to $DESTINATION_DIR"
+fi
+
+# Backup .nvim config
+if [ -d "$SOURCE_NVIM" ]; then
+  cp -r "$SOURCE_NVIM" "$DESTINATION_DIR/custom_nvim"
+  echo "Backed up nvim/lua/custom to $DESTINATION_DIR"
+fi
+
+echo "Backup completed successfully"
+
--- config_backups/.zshrc
+++ config_backups/.zshrc
@@ -24,3 +24,7 @@ alias lt='exa --tree --level=2'                                         # tree
 
 # after `forgit` was loaded
 export PATH="$PATH:$FORGIT_INSTALL_DIR/bin"
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "/opt/homebrew/opt/nvm/nvm.sh" ] && \. "/opt/homebrew/opt/nvm/nvm.sh"  # This loads nvm
+[ -s "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm" ] && \. "/opt/homebrew/opt/nvm/etc/bash_completion.d/nvm"  # This loads nvm bash_completion
