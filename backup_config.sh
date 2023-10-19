fd4874cf389a5c4da055c1550f48962a294c8172
Chaipat J. <work.chaipat@gmail.com>
2023-10-20 01:07:55 +0700

    update

--- backup_config.sh
+++ backup_config.sh
@@ -1,16 +1,9 @@
 #!/bin/bash
 
-# Script: backup_configs.sh
-# Description: A script to backup zsh configuration files.
-# Author: AppleBoiy
-# Date: August 14, 2023
-
 # Define source and destination paths for configuration files
 SOURCE_ZSHRC=~/.zshrc
 SOURCE_P10K=~/.p10k.sh
-SOURCE_NVIM=~/.config/nvim/lua/custom
-DESTINATION_DIR=./config_backups
-
+DESTINATION_DIR=./shell
 # Create a directory to store the backup files if it doesn't exist
 mkdir -p "$DESTINATION_DIR"
 
@@ -26,11 +19,6 @@ if [ -f "$SOURCE_P10K" ]; then
   echo "Backed up .p10k.sh to $DESTINATION_DIR"
 fi
 
-# Backup .nvim config
-if [ -d "$SOURCE_NVIM" ]; then
-  cp -r "$SOURCE_NVIM" "$DESTINATION_DIR/nvim/custom"
-  echo "Backed up nvim/lua/custom to $DESTINATION_DIR/nvim/custom/"
-fi
 
 echo "Backup completed successfully"
 
