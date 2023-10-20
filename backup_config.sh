40109632bc9cd94dd62e1434409b2b7160c8d459
Chaipat J. <work.chaipat@gmail.com>
2023-10-20 15:50:12 +0700

    / update

--- backup_config.sh
+++ backup_config.sh
@@ -1,24 +1,20 @@
 #!/bin/bash
 
-# Define source paths for configuration files
 SOURCE_ZSHRC=~/.zshrc
 SOURCE_P10K=~/.p10k.sh
 
-# Define destination directories for each operating system
+
 DESTINATIONS=("./home/linux" "./home/macos" "./home/windows")
 
-# Prompt the user for the operating system
+
 echo "Select the operating system for which you want to back up configuration files:"
 echo "1. Linux"
 echo "2. macOS"
 read -p "Enter the number (1/2): " OS_INDEX
 
-# Check if the user's input is a valid index
-if [ "$OS_INDEX" -ge 1 ] && [ "$OS_INDEX" -le ${#DESTINATIONS[@]} ]; then
-  # Set the destination directory based on the OS index
-  DESTINATION_DIR="${DESTINATIONS[OS_INDEX - 1]}"
 
-  # Create the destination directory if it doesn't exist
+if [ "$OS_INDEX" -ge 1 ] && [ "$OS_INDEX" -le ${#DESTINATIONS[@]} ]; then
+  DESTINATION_DIR="${DESTINATIONS[OS_INDEX - 1]}"
   mkdir -p "$DESTINATION_DIR"
 
   # Backup .zshrc
