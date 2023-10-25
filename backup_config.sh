9bb302f286b355d674d2473cef83718772587099
Chaipat J. <work.chaipat@gmail.com>
2023-10-26 01:37:56 +0700

    ~ Backup 26/10/23

--- backup_config.sh
+++ backup_config.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
-SOURCE_ZSHRC=~/.zshrc
-SOURCE_P10K=~/.p10k.sh
+ZSHRC=~/.zshrc
+P10K=~/.p10k.zsh
+BASHRC=~/.bashrc
 
-DESTINATIONS=("./home/ubuntu" "./home/macos")
+DEST=("./home/ubuntu" "./home/macos")
 
 
 echo "Select the operating system for which you want to back up configuration files:"
@@ -12,20 +13,26 @@ echo "2. macOS"
 read -p "Enter the number (1/2): " OS_INDEX
 
 
-if [ "$OS_INDEX" -ge 1 ] && [ "$OS_INDEX" -le ${#DESTINATIONS[@]} ]; then
-  DESTINATION_DIR="${DESTINATIONS[OS_INDEX - 1]}"
+if [ "$OS_INDEX" -ge 1 ] && [ "$OS_INDEX" -le ${#DEST[@]} ]; then
+  DESTINATION_DIR="${DEST[OS_INDEX - 1]}"
   mkdir -p "$DESTINATION_DIR"
 
   # Backup .zshrc
-  if [ -f "$SOURCE_ZSHRC" ]; then
-    cp "$SOURCE_ZSHRC" "$DESTINATION_DIR"
+  if [ -f "$ZSHRC" ]; then
+    cp "$ZSHRC" "$DESTINATION_DIR"
     echo "Backed up .zshrc to $DESTINATION_DIR"
   fi
 
-  # Backup .p10k.sh
-  if [ -f "$SOURCE_P10K" ]; then
-    cp "$SOURCE_P10K" "$DESTINATION_DIR"
-    echo "Backed up .p10k.sh to $DESTINATION_DIR"
+  # Backup .p10k.zsh
+  if [ -f "$P10K" ]; then
+    cp "$P10K" "$DESTINATION_DIR"
+    echo "Backed up .p10k.zsh to $DESTINATION_DIR"
+  fi
+
+  # Backup .bashrc
+  if [ -f "$BASHRC" ]; then
+    cp "$BASHRC" "$DESTINATION_DIR"
+    echo "Backed up .bashrc to $DESTINATION_DIR"
   fi
 
   echo "Backup completed successfully."
