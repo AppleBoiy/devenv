711038a2d4b2cda6d6faa020b7d537ce17e734df
Chaipat J. <work.chaipat@gmail.com>
2023-11-04 15:39:53 +0700

    Backed up configuration files on 2023-11-04 15:39:53

--- backup.sh
+++ backup.sh
@@ -7,41 +7,43 @@ handle_error() {
 
 backup_configs() {
   local dest_dir=""
-  
+
   case "$1" in
-    ubuntu)
-      dest_dir="./home/ubuntu"
-      ;;
-    macos)
-      dest_dir="./home/macos"
-      ;;
-    *)
-      handle_error "Invalid operating system. Please specify either 'ubuntu' or 'macos'."
-      ;;
+    ubuntu) dest_dir="./home/ubuntu" ;;
+    macos) dest_dir="./home/macos" ;;
+    *) handle_error "Invalid operating system. Please specify either 'ubuntu' or 'macos'."
   esac
 
-  if [ ! -d "$dest_dir" ]; then
-    mkdir -p "$dest_dir" || handle_error "Failed to create destination directory."
-  fi
+  [ ! -d "$dest_dir" ] && mkdir -p "$dest_dir" || :
 
   files_to_backup=("$HOME/.zshrc" "$HOME/.p10k.zsh" "$HOME/.bashrc")
 
   for file in "${files_to_backup[@]}"; do
-    if [ -f "$file" ]; then
-      cp "$file" "$dest_dir" || handle_error "Failed to back up $file."
-      echo "Backed up $file to $dest_dir"
-    fi
+    [ -f "$file" ] && cp "$file" "$dest_dir" || :
   done
 
   time=$(date +"%Y-%m-%d %H:%M:%S")
   git add . && git commit -m "Backed up configuration files on $time" && git push || handle_error "Failed to commit changes to Git."
-  
+
   echo "Backup completed successfully."
 }
 
-if [ "$#" -ne 1 ]; then
-  handle_error "Usage: $0 <os> (os: 'ubuntu' or 'macos')"
+show_available_os() {
+  echo "Usage: $0 -o <os> (os: 'ubuntu' or 'macos')"
+  echo "Available OS:"
+  echo "  -o ubuntu : Choose Ubuntu"
+  echo "  -o macos  : Choose macOS"
+}
+
+if [ "$#" -eq 0 ]; then
+  backup_configs "macos"
 else
-  backup_configs "$1"
+  while getopts "o:h" opt; do
+    case $opt in
+      o) backup_configs "$OPTARG" ;;
+      h) show_available_os; exit 0 ;;
+      *) handle_error "Usage: $0 -o <os> (os: 'ubuntu' or 'macos')" ;;
+    esac
+  done
 fi
 
