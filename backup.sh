bcebe8394ed3b3ac5f04dee0cb83d057b6e437aa
Chaipat J. <work.chaipat@gmail.com>
2023-11-04 15:26:40 +0700

    Backed up configuration files on 2023-11-04 15:26:40

--- backup.sh
+++ backup.sh
@@ -1,46 +1,47 @@
 #!/bin/bash
 
-bash homebrew/list_installed_packages.sh
+handle_error() {
+  echo "Error: $1"
+  exit 1
+}
 
-ZSHRC=~/.zshrc
-P10K=~/.p10k.zsh
-BASHRC=~/.bashrc
+backup_configs() {
+  local dest_dir=""
+  
+  case "$1" in
+    ubuntu)
+      dest_dir="./home/ubuntu"
+      ;;
+    macos)
+      dest_dir="./home/macos"
+      ;;
+    *)
+      handle_error "Invalid operating system. Please specify either 'ubuntu' or 'macos'."
+      ;;
+  esac
 
-DEST=("./home/ubuntu" "./home/macos")
-
-echo "Select the operating system for which you want to back up configuration files:"
-echo "1. Ubuntu"
-echo "2. macOS"
-read -p "Enter the number (1/2): " OS_INDEX
-
-if [ "$OS_INDEX" -ge 1 ] && [ "$OS_INDEX" -le ${#DEST[@]} ]; then
-  DESTINATION_DIR="${DEST[OS_INDEX - 1]}"
-  mkdir -p "$DESTINATION_DIR"
-
-  # Backup .zshrc
-  if [ -f "$ZSHRC" ]; then
-    cp "$ZSHRC" "$DESTINATION_DIR"
-    echo "Backed up .zshrc to $DESTINATION_DIR"
+  if [ ! -d "$dest_dir" ]; then
+    mkdir -p "$dest_dir" || handle_error "Failed to create destination directory."
   fi
 
-  # Backup .p10k.zsh
-  if [ -f "$P10K" ]; then
-    cp "$P10K" "$DESTINATION_DIR"
-    echo "Backed up .p10k.zsh to $DESTINATION_DIR"
-  fi
+  files_to_backup=("$HOME/.zshrc" "$HOME/.p10k.zsh" "$HOME/.bashrc")
 
-  # Backup .bashrc
-  if [ -f "$BASHRC" ]; then
-    cp "$BASHRC" "$DESTINATION_DIR"
-    echo "Backed up .bashrc to $DESTINATION_DIR"
-  fi
+  for file in "${files_to_backup[@]}"; do
+    if [ -f "$file" ]; then
+      cp "$file" "$dest_dir" || handle_error "Failed to back up $file."
+      echo "Backed up $file to $dest_dir"
+    fi
+  done
 
-  TIME=$(date +"%Y-%m-%d %H:%M:%S")
-  git add .
-  git commit -m "Backed up configuration files on $TIME"
-  git push
+  time=$(date +"%Y-%m-%d %H:%M:%S")
+  git add . && git commit -m "Backed up configuration files on $time" && git push || handle_error "Failed to commit changes to Git."
+  
   echo "Backup completed successfully."
+}
+
+if [ "$#" -ne 1 ]; then
+  handle_error "Usage: $0 <os> (os: 'ubuntu' or 'macos')"
 else
-  echo "Invalid choice. Please enter a valid number (1/2)."
+  backup_configs "$1"
 fi
 
